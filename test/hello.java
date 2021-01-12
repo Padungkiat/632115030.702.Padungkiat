73041e030d109e118dc24ff64de6fb953d9b8094
Padungkiat <padungkiat_leksri@elearning.cmu.ac.th>
2021-01-12 21:24:05 +0700

    3 hello

--- test/hello.java
+++ test/hello.java
@@ -2,6 +2,6 @@ package test;
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Sin at CAMT");
+        System.out.println("Hello from Sin at CAMT, CMU");
     }
 }
